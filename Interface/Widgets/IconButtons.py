86671102f35588cda6e021f39372b96c2217eb62
Snackhole <37544796+Snackhole@users.noreply.github.com>
2021-11-28 19:13:27 -0600

    More icon buttons

--- Interface/Widgets/IconButtons.py
+++ Interface/Widgets/IconButtons.py
@@ -19,6 +19,22 @@ class AddButton(QPushButton):
         self.setIcon(QIcon(IconPixmap))
 
 
+class CopyButton(QPushButton):
+    def __init__(self, Slot, Tooltip="Copy"):
+        super().__init__()
+
+        self.CreateIcon()
+
+        self.setToolTip(Tooltip)
+
+        self.clicked.connect(Slot)
+
+    def CreateIcon(self):
+        IconPixmap = QPixmap()
+        IconPixmap.loadFromData(QtCore.QByteArray.fromBase64(b"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAf0lEQVQ4jWP8////fwbiASO6ShYQYVMvRtCEI42vMNQdbXoNMQAElGyFcGq+d/gdTjkmEpxPGwNYiFADB0caX6EGeCMeA9D9jS2gQQGL1wXEBCz1wgBfVBE0AOQXdEBM4oIZwIiuGCO08YBBnpCICVhQdmZgZETNpURncQYGBgDHRSyJhgjGSwAAAABJRU5ErkJggg=="))
+        self.setIcon(QIcon(IconPixmap))
+
+
 class DamageButton(QPushButton):
     def __init__(self, Slot, Tooltip="Damage"):
         super().__init__()
@@ -32,6 +48,22 @@ class DamageButton(QPushButton):
     def CreateIcon(self):
         IconPixmap = QPixmap()
         IconPixmap.loadFromData(QtCore.QByteArray.fromBase64(b"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAtElEQVQ4jZ2S4Q2FIAyEr4RNXKNzOIwDOIxzdA1n4f14FE8oPvMuIVSgXy5npZRSMEqCs1AZAESu95Xn0J+g7IWZgWEEeoRk/lDVG+gVpFQBKGbWdq9JiFbq6WYGVW015ROFjURObo1vIQ6QfyGJ6hACfMPd1iWEMCCEeDOAECLxINZLkdbM2o/Tf/HgYHCyH+dwyU5mgEcIOwiHo1tt0LZ1GYZrlkGvlgm9F2Ae4hTCzQDwAWgNwjiDIS4PAAAAAElFTkSuQmCC"))
+        self.setIcon(QIcon(IconPixmap))
+
+
+class DeleteButton(QPushButton):
+    def __init__(self, Slot, Tooltip="Delete"):
+        super().__init__()
+
+        self.CreateIcon()
+
+        self.setToolTip(Tooltip)
+
+        self.clicked.connect(Slot)
+
+    def CreateIcon(self):
+        IconPixmap = QPixmap()
+        IconPixmap.loadFromData(QtCore.QByteArray.fromBase64(b"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAQ0lEQVQ4jWMYBQyM/////09JMLCAiHoxMbI0N756BTEABGyFhEjSfPjdOzDNRJbVSGDgDYCHAcxPpAKKo3HEAwYGBgADWhCsPEB/zAAAAABJRU5ErkJggg=="))
         self.setIcon(QIcon(IconPixmap))
 
 
@@ -64,6 +96,38 @@ class HealButton(QPushButton):
     def CreateIcon(self):
         IconPixmap = QPixmap()
         IconPixmap.loadFromData(QtCore.QByteArray.fromBase64(b"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAkElEQVQ4jd2SOw7DMAxDH4NuyWV6/0Okl8nMDo0MxfBvLqeYIB9kR7JN0uMAaOKz1WVJSMpezwfgVZfPfSe+Y7qOb0Dyfcqh0Pu6AJp+gdt2qzxTQLZ5dKx/AdguL76i/Bc27q1aheQyoLjCEqQulysAfI5jCMnlyD4AI0ivDL9NbE1b1juAeewVQIHkbCv0Be5ga3xiirSUAAAAAElFTkSuQmCC"))
+        self.setIcon(QIcon(IconPixmap))
+
+
+class MoveDownButton(QPushButton):
+    def __init__(self, Slot, Tooltip="Move Down"):
+        super().__init__()
+
+        self.CreateIcon()
+
+        self.setToolTip(Tooltip)
+
+        self.clicked.connect(Slot)
+
+    def CreateIcon(self):
+        IconPixmap = QPixmap()
+        IconPixmap.loadFromData(QtCore.QByteArray.fromBase64(b"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAj0lEQVQ4jWP8//8/AxrAEEADjMhcFmwqxBTqsep89aARQwyrASAg5GWLwn+37TBWdUwEnEsQDAMDWIiINnSAoh4cC7iiDR2gqwNFKzwa0aMNHeCKViaYSbjiGRsAqYUlKiZY0iTWEGTNIL2wWCDKEHTNcC8QYwg2zWAGrtyILcTRNeMyAG4IFoCimYGBgQEAtDVEATWWeH0AAAAASUVORK5CYII="))
+        self.setIcon(QIcon(IconPixmap))
+
+
+class MoveUpButton(QPushButton):
+    def __init__(self, Slot, Tooltip="Move Up"):
+        super().__init__()
+
+        self.CreateIcon()
+
+        self.setToolTip(Tooltip)
+
+        self.clicked.connect(Slot)
+
+    def CreateIcon(self):
+        IconPixmap = QPixmap()
+        IconPixmap.loadFromData(QtCore.QByteArray.fromBase64(b"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAkklEQVQ4jWP8//8/AxaAVZCBgYERXYAFl2YxhXoUwVcPGmFyKIaguwCuWcjLFsWAd9sOwwxBcQkTMZpBACSG5Cq4rUzEaMZnCBOxmnEZwkSKZmyGwGMBFEjoipABujwMgA1ACl0GBixRCAPo6mAuQE8cuBIRDKCoZ8KrlAgwDAzAlpnAAFe0oQNs2Zn4WGBgYAAAciE1ohhWgxkAAAAASUVORK5CYII="))
         self.setIcon(QIcon(IconPixmap))
 
 
