2ec01448ba1b43c1502a889937db972b44d12e15
Snackhole <snackhole.dev@gmail.com>
2021-12-01 15:55:12 -0600

    Concentrating toggle button

--- Interface/Widgets/ToggleButtons.py
+++ Interface/Widgets/ToggleButtons.py
@@ -13,3 +13,17 @@ class InspirationButton(QPushButton):
         self.setCheckable(True)
         self.setStyleSheet("QPushButton {font-size: 16pt;}\nQPushButton:checked {background-color: darkgreen;}")
         self.clicked.connect(lambda: self.CharacterWindow.UpdateStat("Inspiration", self.isChecked()))
+
+
+class ConcentratingButton(QPushButton):
+    def __init__(self, CharacterWindow):
+        # Initialize
+        super().__init__("Concentrating")
+
+        # Store Parameters
+        self.CharacterWindow = CharacterWindow
+
+        # Configure
+        self.setCheckable(True)
+        self.setStyleSheet("QPushButton {font-size: 16pt;}\nQPushButton:checked {background-color: darkgreen;}")
+        self.clicked.connect(lambda: self.CharacterWindow.UpdateStat("Concentrating", self.isChecked()))
